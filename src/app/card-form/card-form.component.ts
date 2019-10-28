38f6e9494297c85ca629c746c22cd5d6669bc212
Javier Acevedo <fj.acevedo@soprasteria.com>
2019-10-28 17:03:44 +0100

    feat: add card form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -6,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-form.component.scss']
 })
 export class CardFormComponent implements OnInit {
-  constructor() {}
+  cardForm: FormGroup;
+  months: string[] = [...Array(12).keys()].map(x => {
+    const month = x + 1;
+    const length = month.toString().length;
+    return length === 1 ? '0' + month : month.toString();
+  });
 
-  ngOnInit() {}
+  years: string[] = [...Array(20).keys()].map(x => {
+    const value = x + 1;
+    const lastYear = new Date().getFullYear() - 1;
+    return (lastYear + value).toString();
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.cardForm = this.fb.group({
+      cardNumber: [],
+      cardName: [],
+      expirationDate: this.fb.group({
+        month: [],
+        year: []
+      }),
+      cvv: []
+    });
+
+    // TODO quitar
+    this.cardForm.valueChanges.subscribe(console.log);
+  }
 }
